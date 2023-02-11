9dd5f655c102c5522110942a2e0633a44af6ae96
Filip Piatkowski <filip.piatkowski.gdy@gmail.com>
2023-02-11 21:58:11 +0100

    jjdzr10bd-6.1

--- BugDoom/src/main/java/com/infoshareacademy/User.java
+++ BugDoom/src/main/java/com/infoshareacademy/User.java
@@ -1,4 +1,21 @@
 package com.infoshareacademy;
 
 public class User {
+    private String username;
+    private String password;
+//Constructor for the User class.
+    User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+//Set getters for them.
+//Returns the username for the User object.
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
