b3300ed4082bc3f8a9ebda19493fdf2e4f015bb0
JewstafiewJakub <jakub.jewstafiew@gmail.com>
2023-02-12 12:57:42 +0100

    Model classes updated with fields

--- BugDoom/src/main/java/com/infoshareacademy/model/ControlPoint.java
+++ BugDoom/src/main/java/com/infoshareacademy/model/ControlPoint.java
@@ -1,5 +1,32 @@
 package com.infoshareacademy.model;
 
 public class ControlPoint {
+    // współrzędne x i y składają się na koordynaty
+    int x;
+    int y;
+    int [][] coordinates;
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int[][] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(int[][] coordinates) {
+        this.coordinates = coordinates;
+    }
 }
--- BugDoom/src/main/java/com/infoshareacademy/model/Track.java
+++ BugDoom/src/main/java/com/infoshareacademy/model/Track.java
@@ -1,5 +1,58 @@
 package com.infoshareacademy.model;
 
 public class Track {
+    private ControlPoint startPoint;
+    private ControlPoint endPoint;
+    private ControlPoint checkpoint;
+    private int length;
+    private String difficulty;
+    private String location;
 
+    public ControlPoint getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(ControlPoint startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public ControlPoint getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(ControlPoint endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public ControlPoint getCheckpoint() {
+        return checkpoint;
+    }
+
+    public void setCheckpoint(ControlPoint checkpoint) {
+        this.checkpoint = checkpoint;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
