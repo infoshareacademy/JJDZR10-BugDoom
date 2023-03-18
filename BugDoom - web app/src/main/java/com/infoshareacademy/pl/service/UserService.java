c63fc8d39bab0f1634776b355fa6abebc79afe3d
JewstafiewJakub <jakub.jewstafiew@gmail.com>
2023-03-18 23:54:58 +0100

    Added currentUser variable and methods that set and return it

--- BugDoom - web app/src/main/java/com/infoshareacademy/pl/service/UserService.java
+++ BugDoom - web app/src/main/java/com/infoshareacademy/pl/service/UserService.java
@@ -10,26 +10,38 @@ import java.util.List;
 public class UserService {
     private static final String USERS_FILE_PATH = FilePathConstants.USERS_FILE_PATH;
     private static final DataService <User> dataService = new DataService<>();
+    private static User currentUser;
 
     public static void addNewUser(User user) throws IOException {
         List<User> users = getAllUsers();
         users.add(user);
         dataService.saveToFile(users, USERS_FILE_PATH);
     }
+
     public static void deleteUser (User userToDelete) throws IOException{
         List<User> users = getAllUsers();
         users.remove(userToDelete);
         dataService.saveToFile(users, USERS_FILE_PATH);
     }
-    public static boolean loginValidation (String name, String password) throws IOException {
+
+    public static boolean loginValidation (String emailAddress, String password) throws IOException {
         List<User> users = dataService.readFromFile(USERS_FILE_PATH, User[].class);
         for (User user : users) {
-            if (user.getName() != null && !user.getName().isEmpty() && user.getName().equals(name) && user.getPassword().equals(password)) {
+            if (user.getUserEmailAddress() != null
+                    && !user.getUserEmailAddress().isEmpty()
+                    && user.getUserEmailAddress().equals(emailAddress)
+                    && user.getPassword().equals(password)) {
+                currentUser = user;
                 return true;
             }
         }
         return false;
     }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
     private static List<User> getAllUsers() throws IOException{
         return new ArrayList<>(dataService.readFromFile(USERS_FILE_PATH, User[].class));
     }
