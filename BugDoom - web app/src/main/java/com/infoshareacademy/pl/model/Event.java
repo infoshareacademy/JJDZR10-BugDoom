11266988dc037591fe6251f9c8efcc2ef949918f
JewstafiewJakub <jakub.jewstafiew@gmail.com>
2023-03-26 21:57:34 +0200

    Event class and EventType enum added + getters and setters to the fields in the Event class added

---
+++ BugDoom - web app/src/main/java/com/infoshareacademy/pl/model/Event.java
@@ -0,0 +1,61 @@
+package com.infoshareacademy.pl.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Event {
+    private long eventId;
+    private List<Track> tracks;
+    private List<User> users;
+    private int eventPrice;
+    private LocalDate eventDate;
+    private EventType eventType;
+
+    public long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(long eventId) {
+        this.eventId = eventId;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public int getEventPrice() {
+        return eventPrice;
+    }
+
+    public void setEventPrice(int eventPrice) {
+        this.eventPrice = eventPrice;
+    }
+
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+}
