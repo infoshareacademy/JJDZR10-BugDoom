515bddfb95facd1379a17a8a69f4eef800f8791e
grzeslukasik <grzeslukasik@github.com>
2023-04-02 09:37:01 +0200

    deleted track fixed

--- BugDoom - web app/src/main/java/com/infoshareacademy/pl/model/Location.java
+++ BugDoom - web app/src/main/java/com/infoshareacademy/pl/model/Location.java
@@ -41,6 +41,39 @@ public class Location {
     public void setText(String text) {
         this.text = text;
     }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "x=" + x +
+                ", y=" + y +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Location location = (Location) o;
+
+        if (Double.compare(location.x, x) != 0) return false;
+        if (Double.compare(location.y, y) != 0) return false;
+        return Objects.equals(text, location.text);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        return result;
+    }
 }
 
 
