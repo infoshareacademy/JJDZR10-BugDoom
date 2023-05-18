79070ba76555eb2ea90a531c49bda186ad3f3baa
JakubGrotha <jakub.grotha@gmail.com>
2023-05-18 10:28:17 +0200

    Returned locationName field in the Location class + regenerated setters, getters, equals, hashCode and toString methods in the same class

--- BugDoom - web app/src/main/java/com/infoshareacademy/pl/model/Location.java
+++ BugDoom - web app/src/main/java/com/infoshareacademy/pl/model/Location.java
@@ -3,6 +3,8 @@ package com.infoshareacademy.pl.model;
 import java.util.Objects;
 
 public class Location {
+
+    private String locationName;
     private double x;
     private double y;
 
@@ -10,21 +12,30 @@ public class Location {
     public Location() {
     }
 
-    public Location(double x, double y) {
+    public Location(String locationName, double x, double y) {
+        this.locationName = locationName;
         this.x = x;
         this.y = y;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
     }
 
     public double getX() {
         return x;
     }
 
-    public double getY() {
-        return y;
-    }
-
     public void setX(double x) {
         this.x = x;
+    }
+
+    public double getY() {
+        return y;
     }
 
     public void setY(double y) {
@@ -35,19 +46,31 @@ public class Location {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         Location location = (Location) o;
-        return Double.compare(location.x, x) == 0 && Double.compare(location.y, y) == 0;
+
+        if (Double.compare(location.x, x) != 0) return false;
+        if (Double.compare(location.y, y) != 0) return false;
+        return Objects.equals(locationName, location.locationName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(x, y);
+        int result;
+        long temp;
+        result = locationName != null ? locationName.hashCode() : 0;
+        temp = Double.doubleToLongBits(x);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 
     @Override
     public String toString() {
         return "Location{" +
-                "x=" + x +
+                "locationName='" + locationName + '\'' +
+                ", x=" + x +
                 ", y=" + y +
                 '}';
     }
