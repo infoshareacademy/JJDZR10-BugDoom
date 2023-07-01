a8cbf11457b3c0376bfaa4bc5352b639ecef8de4
JakubGrotha <jakub.grotha@gmail.com>
2023-07-01 23:40:00 +0200

    Solved minor problem

--- BugDoom - web app/src/main/java/com/infoshareacademy/pl/model/Event.java
+++ BugDoom - web app/src/main/java/com/infoshareacademy/pl/model/Event.java
@@ -23,7 +23,7 @@ public class Event {
     @Column(name = "event_description", nullable = false)
     private String eventDescription;
 
-    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "event")
     private List<Track> tracks;
 
     @OneToMany(mappedBy = "event")
